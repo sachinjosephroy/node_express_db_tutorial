13dcd3de0dbff3cec151ca0c5783253e284187df
Sachin Roy <sachin.jr@gmail.com>
2020-08-31 22:04:03 -0400

    modofoed migrations

--- migrations/20200831142235_create-lessons-table.js
+++ migrations/20200831142235_create-lessons-table.js
@@ -17,6 +17,7 @@ exports.up = function(knex) {
             //Foreign key info to 'lessons' table
             tbl.integer('lesson_id')
                 .unsigned()
+                .notNullable()
                 .references('id')
                 .inTable('lessons')
                 .onDelete('CASCADE')
