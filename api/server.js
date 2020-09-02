7776dbdbe780c687b52503380c96c27e86a3262c
Sachin Roy <sachin.jr@gmail.com>
2020-09-01 20:23:52 -0400

    installed and added cors

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const lessonsRouter = require('../Routes/lessons-routes');
 const messagesRouter = require('../Routes/messages-routes');
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use('/api/lessons', lessonsRouter);
 server.use('/api/messages', messagesRouter);
